9b2b97f3b9f92b897e3f76022f8bc6ccb36f46f2
VaishnaviMankala19 <131646799+VaishnaviMankala19@users.noreply.github.com>
2023-04-24 20:22:30 +0530

    Add files via upload

--- bmi.js
+++ bmi.js
@@ -4,7 +4,7 @@ function caluculatebmi()
     const weight=document.getElementById("weight").value;
     const bmi=weight/(height*height);
     const result=document.getElementById("result");
-    result.innerHTML=bmi.toFixed(2);
+    result.innerHTML=`Your BMI is : ${bmi.toFixed(2)}`;
 }
 
 const button=document.getElementById("calculate");
--- temp.js
+++ temp.js
@@ -1,9 +1,12 @@
 function calculate()
 {
-    const celcius=document.getElementById("celcius").value;
+    const celcius=parseFloat(document.getElementById("celcius").value);
     const result=((9/5)*celcius)+32;
+    const result2=celcius+273.15;
     const res=document.getElementById("result");
-    res.innerHTML=result;
+    res.innerHTML= "Farenheit: "+result;
+    const r=document.getElementById("result2");
+    r.innerHTML="Kelvin: "+result2;
 }
 const button=document.getElementById("calculate");
 button.addEventListener("click",calculate);
